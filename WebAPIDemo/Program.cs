f21cfa96bc358aab3e5972652ef10660b00cac1b
raghu <subamraghu11154@gmail.com>
2024-03-15 12:05:05 +0530

    db connected

--- WebAPIDemo/Program.cs
+++ WebAPIDemo/Program.cs
@@ -1,9 +1,7 @@
-/*var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-
-
 
 var app = builder.Build();
 
@@ -14,25 +12,4 @@ app.MapControllers();
 
 app.Run();
 
-*/
 
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-
-namespace WebAPIDemo
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
